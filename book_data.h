875430dac03f3200c85c8e456078915d9684dca2
Vadym Priadun <pryadun.vv06@gmail.com>
2024-01-16 13:03:44 +0200

    some issues hav been corrected

--- book_data.h
+++ book_data.h
@@ -4,26 +4,24 @@
 #include "library.h"
 
 vector<Book> books = {
-    Book("The C++ Programming Language", 49.99, "Bjarne Stroustrup", "Addison-Wesley", 1985, 1348, TECHNICAL, "D:\\Practice of myself\\Library\\books\\txt\\The_C++_Programming_Language.txt"),
-    Book("To Kill a Mockingbird", 14.99, "Harper Lee", "J.B. Lippincott & Co.", 1960, 281, FICTION, "D:\\Practice of myself\\Library\\books\\txt\\mockingbird.txt"),
-    Book("1984", 19.99, "George Orwell", "Secker & Warburg", 1949, 328, SCIENCE_FICTION, "D:\\Practice of myself\\Library\\books\\txt\\1984.txt"),
-    Book("The Great Gatsby", 9.99, "F. Scott Fitzgerald", "Charles Scribner's Sons", 1925, 180, FICTION, "D:\\Practice of myself\\Library\\books\\txt\\the-great-gatsby.txt"),
-    Book("The Hobbit", 12.99, "J.R.R. Tolkien", "Allen & Unwin", 1937, 310, FANTASY, "D:\\Practice of myself\\Library\\books\\txt\\TheHobbit-J.R.R.Tolkien.txt"),
-    Book("Pride and Prejudice", 8.99, "Jane Austen", "T. Egerton", 1813, 279, ROMANCE, "D:\\Practice of myself\\Library\\books\\txt\\pride-and-prejudice.txt"),
-    Book("War and Peace", 24.99, "Leo Tolstoy", "The Russian Messenger", 1869, 1225, HISTORY, "D:\\Practice of myself\\Library\\books\\txt\\war-and-peace.txt"),
-    Book("The Catcher in the Rye", 11.99, "J.D. Salinger", "Little, Brown and Company", 1951, 214, FICTION, "D:\\Practice of myself\\Library\\books\\txt\\Catcher in the Rye.txt"),
-    Book("The Lord of the Rings", 29.99, "J.R.R. Tolkien", "George Allen & Unwin", 1954, 1037, FANTASY, "D:\\Practice of myself\\Library\\books\\txt\\Tolkien-J.-The-lord-of-the-rings-HarperCollins-ebooks-2010.txt"),
-    Book("Moby-Dick", 16.99, "Herman Melville", "Richard Bentley", 1851, 635, FICTION, "D:\\Practice of myself\\Library\\books\\txt\\moby-dick.txt"),
-    Book("The Da Vinci Code", 18.99, "Dan Brown", "Doubleday", 2003, 454, MYSTERY, "D:\\Practice of myself\\Library\\books\\txt\\The_Da_Vinci_Code.txt"),
-    Book("Brave New World", 15.99, "Aldous Huxley", "Chatto & Windus", 1932, 311, SCIENCE_FICTION, "D:\\Practice of myself\\Library\\books\\txt\\Brave-New-World_-_Aldous-Huxley.txt"),
-    Book("The Odyssey", 13.99, "Homer", "Unknown", -800, 384, FANTASY, "D:\\Practice of myself\\Library\\books\\txt\\the-odyssey.txt"),
-    Book("Crime and Punishment", 20.99, "Fyodor Dostoevsky", "The Russian Messenger", 1866, 671, FICTION, "D:\\Practice of myself\\Library\\books\\txt\\crime-and-punishment.txt"),
-    Book("The Shining", 17.99, "Stephen King", "Doubleday", 1977, 447, HORROR, "D:\\Practice of myself\\Library\\books\\txt\\The-Shining.txt"),
-    Book("The Alchemist", 10.99, "Paulo Coelho", "Rocco", 1988, 163, FICTION, "D:\\Practice of myself\\Library\\books\\txt\\_OceanofPDF.com_The_Alchemist.txt"),
-    Book("Frankenstein", 14.99, "Mary Shelley", "Lackington, Hughes, Harding, Mavor & Jones", 1818, 280, HORROR, "D:\\Practice of myself\\Library\\books\\txt\\frankenstein.txt"),
-    Book("One Hundred Years of Solitude", 22.99, "Gabriel Garcia Marquez", "Editorial Sudamericana", 1967, 417, MAGIC_REALISM, "D:\\Practice of myself\\Library\\books\\txt\\One_Hundred_Years_of_Solitude.txt"),
-    Book("The Art of War", 7.99, "Sun Tzu", "Unknown", -500, 60, MILITARY, "D:\\Practice of myself\\Library\\books\\txt\\ArtOfWar.txt"),
-    Book("The Road", 16.99, "Cormac McCarthy", "Alfred A. Knopf", 2006, 287, FICTION, "D:\\Practice of myself\\Library\\books\\txt\\the_road_-_text.txt")
+    Book("The C++ Programming Language", 49.99, "Bjarne Stroustrup", "Addison-Wesley", 1985, 1348, TECHNICAL, R"(D:\Practice of myself\Library\books\txt\The_C++_Programming_Language.txt)"),
+    Book("To Kill a Mockingbird", 14.99, "Harper Lee", "J.B. Lippincott & Co.", 1960, 281, FICTION, R"(D:\Practice of myself\Library\books\txt\mockingbird.txt)"),
+    Book("1984", 19.99, "George Orwell", "Secker & Warburg", 1949, 328, SCIENCE_FICTION, R"(D:\Practice of myself\Library\books\txt\1984.txt)"),
+    Book("The Great Gatsby", 9.99, "F. Scott Fitzgerald", "Charles Scribner's Sons", 1925, 180, FICTION, R"(D:\Practice of myself\Library\books\txt\the-great-gatsby.txt)"),
+    Book("The Hobbit", 12.99, "J.R.R. Tolkien", "Allen & Unwin", 1937, 310, FANTASY, R"(D:\Practice of myself\Library\books\txt\TheHobbit-J.R.R.Tolkien.txt)"),
+    Book("Pride and Prejudice", 8.99, "Jane Austen", "T. Egerton", 1813, 279, ROMANCE, R"(D:\Practice of myself\Library\books\txt\pride-and-prejudice.txt)"),
+    Book("The Catcher in the Rye", 11.99, "J.D. Salinger", "Little, Brown and Company", 1951, 214, FICTION, R"(D:\Practice of myself\Library\books\txt\Catcher in the Rye.txt)"),
+    Book("The Lord of the Rings", 29.99, "J.R.R. Tolkien", "George Allen & Unwin", 1954, 1037, FANTASY, R"(D:\Practice of myself\Library\books\txt\Tolkien-J.-The-lord-of-the-rings-HarperCollins-ebooks-2010.txt)"),
+    Book("Moby-Dick", 16.99, "Herman Melville", "Richard Bentley", 1851, 635, FICTION, R"(D:\Practice of myself\Library\books\txt\moby-dick.txt)"),
+    Book("The Da Vinci Code", 18.99, "Dan Brown", "Doubleday", 2003, 454, MYSTERY, R"(D:\Practice of myself\Library\books\txt\The_Da_Vinci_Code.txt)"),
+    Book("Brave New World", 15.99, "Aldous Huxley", "Chatto & Windus", 1932, 311, SCIENCE_FICTION, R"(D:\Practice of myself\Library\books\txt\Brave-New-World_-_Aldous-Huxley.txt)"),
+    Book("The Odyssey", 13.99, "Homer", "Unknown", -800, 384, FANTASY, R"(D:\Practice of myself\Library\books\txt\the-odyssey.txt)"),
+    Book("The Shining", 17.99, "Stephen King", "Doubleday", 1977, 447, HORROR, R"(D:\Practice of myself\Library\books\txt\The-Shining.txt)"),
+    Book("The Alchemist", 10.99, "Paulo Coelho", "Rocco", 1988, 163, FICTION, R"(D:\Practice of myself\Library\books\txt\_OceanofPDF.com_The_Alchemist.txt)"),
+    Book("Frankenstein", 14.99, "Mary Shelley", "Lackington, Hughes, Harding, Mavor & Jones", 1818, 280, HORROR, R"(D:\Practice of myself\Library\books\txt\frankenstein.txt)"),
+    Book("One Hundred Years of Solitude", 22.99, "Gabriel Garcia Marquez", "Editorial Sudamericana", 1967, 417, MAGIC_REALISM, R"(D:\Practice of myself\Library\books\txt\One_Hundred_Years_of_Solitude.txt)"),
+    Book("The Art of War", 7.99, "Sun Tzu", "Unknown", -500, 60, MILITARY, R"(D:\Practice of myself\Library\books\txt\ArtOfWar.txt)"),
+    Book("The Road", 16.99, "Cormac McCarthy", "Alfred A. Knopf", 2006, 287, FICTION, R"(D:\Practice of myself\Library\books\txt\the_road_-_text.txt)")
 };
 
 #endif
